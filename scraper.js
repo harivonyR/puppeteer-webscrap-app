fa6074edda7da4d3d32871cb23ed522a6ef769d2
TensorEleven <harivonyratefiarison@gmail.com>
2022-03-10 13:21:13 +0300

    UPDATE scrap simple title

--- scraper.js
+++ scraper.js
@@ -2,9 +2,24 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch();
+  console.log('browser ok ..');
+
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'example.png' });
+  console.log('page ok ..')
+
+  await page.goto('https://marketingplatform.google.com/about/partners/find-a-partner');
+  console.log('page target ok ..')
+  // take screen shot
+  // await page.screenshot({ path: 'example.png' });
+
+
+    const titleList = await page.evaluate(
+        ()=> Array.from(document.querySelectorAll('h3 a')).map((partner)=>partner.innerText) // cf difference innerHtml/textContent/
+    )
+    console.log('scrap ok ..')
+
+
+    console.log(titleList)
 
   await browser.close();
 })();
