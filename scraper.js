e6ab5b7f63e3c938ad9b26f6e58dda413da8577a
TensorEleven <harivonyratefiarison@gmail.com>
2022-03-14 10:18:47 +0300

    UPDATE sparse map, ISSUE unstable returned data

--- scraper.js
+++ scraper.js
@@ -1,30 +1,33 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  console.log('[👍] browser .. ');
+    const browser = await puppeteer.launch({ headless: true });
+    console.log('[👍] browser .. ');
 
-  const page = await browser.newPage();
-  console.log('[👍] page  ..');
+    const page = await browser.newPage();
+    console.log('[👍] page  ..');
 
-  await page.goto('https://marketingplatform.google.com/about/partners/find-a-partner');
-  console.log('[👍] page target ..');
-  // take screen shot
-  // await page.screenshot({ path: 'example.png' });
+    await page.goto('https://marketingplatform.google.com/about/partners/find-a-partner');
+    console.log('[👍] page target ..');
 
     const partners = await page.evaluate(
         ()=> Array.from(document.querySelectorAll('.inner-container'))
         
-        .map(partner=>({
-            title : partner.querySelector('h3.title').innerText,
-            partnerType : partner.querySelector('li a').innerText
-            //logo : partner.querySelector('.logo-wrapper img').src
-            })
+        .map(partner=>{
+            let data = {
+                title : partner.querySelector('h3.title').innerText,
+                link : partner.querySelector('h3.title a').href,
+                partnerType : partner.querySelector('li a').innerText.trim(),
+                logo : partner.querySelector('.logo-wrapper img').src
+                }
+            return data
+            }
         )
     )
 
     console.log('[👍] scrap ok');
 
+    // log received data
     console.log(partners);
 
     await browser.close();
