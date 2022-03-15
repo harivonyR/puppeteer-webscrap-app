da5f3718105f98b200c95b5fd67a58d15012fed0
TensorEleven <harivonyratefiarison@gmail.com>
2022-03-15 15:29:30 +0300

    CHECK selection done error throw ISSUE user must click on x button

--- scraper.js
+++ scraper.js
@@ -1,31 +1,62 @@
 const puppeteer = require('puppeteer');
 //const cheerio = require ('cheerio')
 
+
+
+
 (async () => {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+        ignoreHTTPSErrors: true,
+        headless: false,
+        devtools: false,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     console.log('[👍] browser .. ');
+
 
     const page = await browser.newPage();
     console.log('[👍] page  ..');
 
-    await page.goto('https://www.property24.com/for-sale/northern-cape/8');
+    await page.goto('https://www.french-property.com/properties-for-sale?currency=EUR&land_size_unit=m%C2%B2',{waitUntil: 'load', timeout : 0});
+    
+    //await new Promise(resolve => setTimeout(resolve, 20000));
+
     console.log('[👍] page target ..');
 
-    let data = [];
+    let res = [];
 
     let proprieties = await page.evaluate(
-        ()=> Array.from(document.querySelectorAll('.p24_regularTile'))
+        ()=> Array.from(document.querySelectorAll('li.property_listing'))
+        .map((propriety)=>{
+            try{
+                let data = {
+                    name : propriety.querySelector('h3 a').innerText,
+                    price : propriety.querySelector('h4').innerText,
+                    region : propriety.querySelector('span.region').innerText
+                    
+                    }
+                return data;
+            }
+            catch(e){
+                console.log(e)
+            }
+        })
     )
+
+    // proprieties = await proprieties.map((propriety)=>{
+    //     return propriety.textContent
+    // })
     
-    proprieties = proprieties.map((propriety)=>{
-        let data = {
-            price :         propriety.querySelector('.p24_price').innerText,
-            img :           propriety.querySelector('span.p24_image > img').src,
-            description :   propriety.querySelector('.p24_excerpt').innerText,
-            location :      propriety.querySelector('.p24_location').innerText
-        }
-        return data;
-    })
+    // proprieties = proprieties.map((propriety)=>{
+    //     let data = {
+    //         name :  propriety.querySelector('h4').innerText
+    //         // price :         propriety.querySelector('.p24_price').innerText,
+    //         // img :           propriety.querySelector('span.p24_image > img').src,
+    //         // description :   propriety.querySelector('.p24_excerpt').innerText,
+    //         // location :      propriety.querySelector('.p24_location').innerText
+    //     }
+    //     return data;
+    // })
     console.log('[👍] scrap ok');
 
     // log received data
---
+++ timesOut.js
@@ -0,0 +1,9 @@
+
+function startPage(){
+    console.log('You have waited too long time')
+}
+
+console.log('StartPage')
+setTimeout(()=>{startPage()},5000)
+
+//console.log('You have waited too long time')
