5ae6038b029f5a5a8d2241fc54ac01957cc53593
TensorEleven <harivonyratefiarison@gmail.com>
2022-03-16 15:30:31 +0300

    CHECK login ok, page browse ok, scrap ok

--- timesOut.js
+++
@@ -1,9 +0,0 @@
-
-function startPage(){
-    console.log('You have waited too long time')
-}
-
-console.log('StartPage')
-setTimeout(()=>{startPage()},5000)
-
-//console.log('You have waited too long time')
---
+++ user.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer');
+const sleep = require('./helper')
+
+async function login (browser){
+    console.log('[👍] login browser .. ');
+
+    const loginPage = await browser.newPage();
+    console.log('[👍] login ready  ..');
+
+    loginPage.goto('https://www.french-property.com/properties-for-sale?start_page=1',{waitUntil: 'load', timeout : 0});
+
+    await new Promise(resolve => setTimeout(resolve, 20000))
+            .then(()=>{
+                loginPage._client.send("Page.stopLoading");
+                console.log('[👍] login open ');
+            });
+    
+    await loginPage.waitForSelector('li.user a')     
+    await loginPage.click('li.user a');
+    
+    await sleep(10000)
+    await loginPage.waitForSelector('#email')
+    await loginPage.type('#email','harivonyratefiarison@gmail.com',{delai:100});
+    
+    await sleep(50000)
+    await loginPage.waitForSelector('#password')
+    await loginPage.type('#password','test123456',{delai:100});
+    await loginPage.keyboard.press('Enter');
+    console.log('[👍] Login Complete ');
+}
+
+module.exports = login;
