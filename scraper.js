3c67abe4b88084ce6851c894b9fb3131167b5dcf
TensorEleven <harivonyratefiarison@gmail.com>
2022-03-25 11:33:01 +0300

    CHECK login bot, select name (waitSelector)

--- scraper.js
+++ scraper.js
@@ -3,73 +3,50 @@ const login = require('./user')
 const saveToCsv = require('./file')
 
 // DATA
-let res = [];
-let link = 'https://www.french-property.com/properties-for-sale?start_page=1';
-let totalPage = 2;
-
+let link = 'https://service.europe.arco.biz/ktmthinclient/Validation.aspx';
 (async () => {
 
 // RUN puppeteer
     const browser = await puppeteer.launch({
-        ignoreHTTPSErrors: false,
-        headless: true,
-        devtools: false,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        headless: false
     });
     console.log('[👍] browser .. ');
 
     const page = await browser.newPage();
-    console.log('[👍] page  ..');
+    console.log('[👍] new page created  ..');
 
-    page.goto(link,{waitUntil: 'load', timeout : 0});
 
 // LOGIN
     await login(browser)
-    console.log('login complete')
-// BROWSE PAGE
-    for(let pageNumber=1; pageNumber<=totalPage; pageNumber++){
-        // page.goto(link+pageNumber,{waitUntil: 'load', timeout : 0});
-    
-        // STOP Browser loading after 20s
-        await new Promise(resolve => setTimeout(resolve, 20000))
-            .then(()=>{
-                page._client.send("Page.stopLoading");
-                console.log('[👍] Page '+pageNumber);
-            });
-    
-    // SCRAP data
-        let properties = await page.evaluate(
-            ()=> Array.from(document.querySelectorAll('li.property_listing'))
-            .map((propertie)=>{
-                try{
-                    let data = {
-                        name : propertie.querySelector('h3 a').innerText,
-                        price : propertie.querySelector('h4').innerText,
-                        region : propertie.querySelector('span.region strong').innerText
-                    }
-                    return data;
-                }
-                catch(e){
-                    console.log("ERR :"+e) ;
-                }
-            })
-        )
 
-    // CONCATENATE the new result 
-        res = [...res,...properties];
-        console.log('[👍] Scrap '+pageNumber);
-        
-        await page.click("li.next a");
-    }
+    await page.goto(link);
+    console.log('[👍] Main page opened')
+
+    // STOP Browser loading after 20s
+    // await new Promise(resolve => setTimeout(resolve, 10000))
+    //     .then(()=>{
+    //         page._client.send("Page.stopLoading");
+    //         console.log('[👍] Page '+pageNumber);
+    //     });
+
+    // SCRAP data
+    await page.waitForSelector('.x-grid3-row-table tr')
+
+    let rows = await page.evaluate(
+            ()=> Array.from(window.document.querySelectorAll('.x-grid3-row-table tr'))
+                .map((row)=>row.querySelector('div.x-grid3-col-name').innerText)
+    )
+    // CONCATENATE the new result
+        //res = [...res,...properties];
     
     // LOG received data
-    console.log(res);
+    console.log(rows);
 
     // Data struggling
-    res = res.filter((e)=>e!=null)
+    //res = res.filter((e)=>e!=null)
 
     // Save to the folder
-    saveToCsv(res,'french_property');
+    //saveToCsv(res,'french_property');
 
     //await browser.close();
 })();
--- user.js
+++ user.js
@@ -2,32 +2,22 @@ const puppeteer = require('puppeteer');
 const sleep = require('./helper')
 
 async function login (browser){
-    console.log('[👍] login browser .. ');
+    console.log('[👍] login browser ');
 
     const loginPage = await browser.newPage();
-    console.log('[👍] login page is openning  ..');
+    console.log('[👍] login page is openning ');
 
-    //loginPage.goto('https://www.french-property.com/properties-for-sale?start_page=1',{waitUntil: 'load', timeout : 0});
+    await loginPage.goto('https://service.europe.arco.biz/ktmthinclient/ValidationLogin.aspx')
+    console.log('[👍] login page opened ');
 
-    loginPage.goto('https://www.french-property.com/login')
-
-    await new Promise(resolve => setTimeout(resolve, 15000))
-            .then(()=>{
-                loginPage._client.send("Page.stopLoading");
-                console.log('[👍] login finish loading ');
-            });
-    
-    // await loginPage.waitForSelector('li.user a')     
-    // await loginPage.click('li.user a');
-    
-    await sleep(10000)
-    await loginPage.waitForSelector('#email')
-    await loginPage.type('#email','harivonyratefiarison@gmail.com',{delai:100});
-    
-    await sleep(10000)
-    await loginPage.waitForSelector('#password')
-    await loginPage.type('#password','test123456',{delai:100});
+    await sleep(4000)
+    //await loginPage.waitForSelector('#userName')
+    await loginPage.type('#userName','SENMAU62',{delai:100});
+    await sleep(3000)
+    //await loginPage.waitForSelector('#userPassword')
+    await loginPage.type('#userPassword','M3rckx',{delai:100});
     await loginPage.keyboard.press('Enter');
+    
     console.log('[👍] Login Done ! ');
 }
 
