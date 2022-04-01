08ea7e83bc8450b338cd34efcdaa8370df46507a
TensorEleven <harivonyratefiarison@gmail.com>
2022-04-01 13:59:31 +0300

    DELETE only minimum file to run

--- server.js
+++ server.js
@@ -15,20 +15,20 @@ app.get('/',  (req, res)=> {
     res.render('index', {})
 });
 
-app.get('/data',async (req,res)=>{
-    // res.send('loading')
-    rows = await mockScrapper.scrap()
-    res.render('data', {rows : rows})
-})
+// app.get('/data',async (req,res)=>{
+//     // res.send('loading')
+//     rows = await mockScrapper.scrap()
+//     res.render('data', {rows : rows})
+// })
 
-app.get('/download', async (req,res)=>{
-    res.download('./public/assets/batch.xls'); 
-})
+// app.get('/download', async (req,res)=>{
+//     res.download('./public/assets/batch.xls'); 
+// })
 
-app.get('/loading',async(req,res)=>{
-    ///let data = await sleep(5000)
-    res.render('loading')
-})
+// app.get('/loading',async(req,res)=>{
+//     ///let data = await sleep(5000)
+//     res.render('loading')
+// })
 
 const port = process.env.PORT || 3535;
 app.listen(port,()=>{
