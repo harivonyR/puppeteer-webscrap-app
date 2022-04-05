2bf9c027786eead1d71a6eb3d1907dcbf4f2e572
TensorEleven <harivonyratefiarison@gmail.com>
2022-04-05 09:13:19 +0300

    UPDATE set timeout 6000

--- scpraping/mockScrapper.js
+++ scpraping/mockScrapper.js
@@ -19,11 +19,11 @@ async function scrap() {
 // LOGIN
     await login(browser)
 
-    await page.goto(link,{waitUntil: 'domcontentloaded', timeout: 35000});
+    await page.goto(link,{waitUntil: 'load', timeout: 35000});
     console.log('[👍] Main page opened')
 
     // SCRAP data
-    await page.waitForSelector('.x-grid3-row-table tr',{ timeout: 0})
+    await page.waitForSelector('.x-grid3-row-table tr',{ timeout: 6000})
     //.then(()=>console.log('selector ok'))
 
     let rows = await page.evaluate(
